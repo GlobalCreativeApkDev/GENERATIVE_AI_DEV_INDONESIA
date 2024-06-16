f3d89135720befcd777db151f240590a0d02a8aa
GlobalCreativeApkDev <globalcreativeapkdev2022@gmail.com>
2024-06-16 17:35:37 +1200

    Update gemini_cli_tactics.py

--- gemini_cli_tactics/gemini_cli_tactics/gemini_cli_tactics.py
+++ gemini_cli_tactics/gemini_cli_tactics/gemini_cli_tactics.py
@@ -135,6 +135,13 @@ def get_elemental_damage_multiplier(element1: str, element2: str) -> mpf:
         return mpf("1")
 
 
+def resistance_accuracy_rule(accuracy: mpf, resistance: mpf) -> mpf:
+    if resistance - accuracy <= mpf("0.15"):
+        return mpf("0.15")
+    else:
+        return resistance - accuracy
+
+
 def load_game_data(file_name):
     # type: (str) -> SavedGameData
     return pickle.load(open(file_name, "rb"))
@@ -210,6 +217,7 @@ class Battle:
         # type: (Team, Team) -> None
         self.team1: Team = team1
         self.team2: Team = team2
+        self.reward: Reward
 
     def __str__(self):
         # type: () -> str
@@ -240,6 +248,10 @@ class BattleArea:
         self.name: str = name
         self.__levels: list = levels
         self.clear_reward: Reward = clear_reward
+
+    def get_levels(self):
+        # type: () -> list
+        return self.__levels
 
     def __str__(self):
         # type: () -> str
@@ -381,11 +393,85 @@ class LegendaryCreatureInventory:
     This class contains attributes of an inventory containing legendary creatures.
     """
 
+    def __init__(self):
+        # type: () -> None
+        self.__legendary_creatures: list = []  # initial value
+
+    def __str__(self):
+        # type: () -> str
+        res: str = str(type(self).__name__) + "("  # initial value
+        index: int = 0  # initial value
+        for item in vars(self).items():
+            res += str(item[0]) + "=" + str(item[1])
+
+            if index < len(vars(self).items()) - 1:
+                res += ", "
+
+            index += 1
+
+        return res + ")"
+
+    def add_legendary_creature(self, legendary_creature):
+        # type: (LegendaryCreature) -> None
+        self.__legendary_creatures.append(legendary_creature)
+
+    def remove_legendary_creature(self, legendary_creature):
+        # type: (LegendaryCreature) -> bool
+        if legendary_creature in self.__legendary_creatures:
+            self.__legendary_creatures.remove(legendary_creature)
+            return True
+        return False
+
+    def get_legendary_creatures(self):
+        # type: () -> list
+        return self.__legendary_creatures
+
+    def clone(self):
+        # type: () -> LegendaryCreatureInventory
+        return copy.deepcopy(self)
+
 
 class ItemInventory:
     """
     This class contains attributes of an inventory containing items.
     """
+
+    def __init__(self):
+        # type: () -> None
+        self.__items: list = []  # initial value
+
+    def __str__(self):
+        # type: () -> str
+        res: str = str(type(self).__name__) + "("  # initial value
+        index: int = 0  # initial value
+        for item in vars(self).items():
+            res += str(item[0]) + "=" + str(item[1])
+
+            if index < len(vars(self).items()) - 1:
+                res += ", "
+
+            index += 1
+
+        return res + ")"
+
+    def add_item(self, item):
+        # type: (Item) -> None
+        self.__items.append(item)
+
+    def remove_item(self, item):
+        # type: (Item) -> bool
+        if item in self.__items:
+            self.__items.remove(item)
+            return True
+        return False
+
+    def get_items(self):
+        # type: () -> list
+        return self.__items
+
+    def clone(self):
+        # type: () -> ItemInventory
+        return copy.deepcopy(self)
 
 
 ###########################################
@@ -403,16 +489,180 @@ class Team:
     This class contains attributes of a team brought to battles.
     """
 
+    MAX_LEGENDARY_CREATURES: int = 5
+
+    def __init__(self, legendary_creatures=None):
+        # type: (list) -> None
+        if legendary_creatures is None:
+            legendary_creatures = []
+        self.__legendary_creatures: list = legendary_creatures if len(legendary_creatures) <= \
+                                                                  self.MAX_LEGENDARY_CREATURES else []
+
+    def add_legendary_creature(self, legendary_creature):
+        # type: (LegendaryCreature) -> bool
+        if len(self.__legendary_creatures) < self.MAX_LEGENDARY_CREATURES:
+            self.__legendary_creatures.append(legendary_creature)
+            return True
+        return False
+
+    def remove_legendary_creature(self, legendary_creature):
+        # type: (LegendaryCreature) -> bool
+        if legendary_creature in self.__legendary_creatures:
+            self.__legendary_creatures.remove(legendary_creature)
+            return True
+        return False
+
+    def get_legendary_creatures(self):
+        # type: () -> list
+        return self.__legendary_creatures
+
+    def __str__(self):
+        # type: () -> str
+        res: str = str(type(self).__name__) + "("  # initial value
+        index: int = 0  # initial value
+        for item in vars(self).items():
+            res += str(item[0]) + "=" + str(item[1])
+
+            if index < len(vars(self).items()) - 1:
+                res += ", "
+
+            index += 1
+
+        return res + ")"
+
+    def clone(self):
+        # type: () -> Team
+        return copy.deepcopy(self)
+
 
 class LegendaryCreature:
     """
     This class contains attributes of a legendary creature in this game.
     """
 
+    MIN_RATING: int = 1
+    MAX_RATING: int = 6
+    MIN_CRIT_RATE: mpf = mpf("0.15")
+    MIN_CRIT_DAMAGE: mpf = mpf("1.5")
+    MIN_RESISTANCE: mpf = mpf("0.15")
+    MAX_RESISTANCE: mpf = mpf("1")
+    MIN_ACCURACY: mpf = mpf("0")
+    MAX_ACCURACY: mpf = mpf("1")
+    MIN_ATTACK_GAUGE: mpf = mpf("0")
+    FULL_ATTACK_GAUGE: mpf = mpf("1")
+    MIN_EXTRA_TURN_CHANCE: mpf = mpf("0")
+    MAX_EXTRA_TURN_CHANCE: mpf = mpf("0.5")
+    MIN_COUNTERATTACK_CHANCE: mpf = mpf("0")
+    MAX_COUNTERATTACK_CHANCE: mpf = mpf("1")
+    MIN_REFLECTED_DAMAGE_PERCENTAGE: mpf = mpf("0")
+    MIN_LIFE_DRAIN_PERCENTAGE: mpf = mpf("0")
+    MIN_CRIT_RESIST: mpf = mpf("0")
+    MAX_CRIT_RESIST: mpf = mpf("1")
+    MIN_BENEFICIAL_EFFECTS: int = 0
+    MAX_BENEFICIAL_EFFECTS: int = 10
+    MIN_HARMFUL_EFFECTS: int = 0
+    MAX_HARMFUL_EFFECTS: int = 10
+    POTENTIAL_ELEMENTS: list = ["TERRA", "FLAME", "SEA", "NATURE", "ELECTRIC", "ICE", "METAL", "DARK", "LIGHT", "WAR",
+                                "PURE", "LEGEND", "PRIMAL", "WIND", "BEAUTY", "MAGIC", "CHAOS", "HAPPY", "DREAM",
+                                "SOUL"]
+    DEFAULT_MAX_HP_PERCENTAGE_UP: mpf = mpf("0")
+    DEFAULT_MAX_MAGIC_POINTS_PERCENTAGE_UP: mpf = mpf("0")
+    DEFAULT_ATTACK_POWER_PERCENTAGE_UP: mpf = mpf("0")
+    DEFAULT_ATTACK_SPEED_PERCENTAGE_UP: mpf = mpf("0")
+    DEFAULT_DEFENSE_PERCENTAGE_UP: mpf = mpf("0")
+    DEFAULT_CRIT_DAMAGE_UP: mpf = mpf("0")
+
+    def __init__(self, name, element, rating, max_hp, max_magic_points, attack_power, defense, attack_speed,
+                 skills, awaken_bonus):
+        # type: (str, str, int, mpf, mpf, mpf, mpf, mpf, list, AwakenBonus) -> None
+        self.legendary_creature_id: str = str(uuid.uuid1())  # generating random legendary creature ID
+        self.name: str = name
+        self.element: str = element if element in self.POTENTIAL_ELEMENTS else self.POTENTIAL_ELEMENTS[0]
+        self.rating: int = rating if self.MIN_RATING <= rating <= self.MAX_RATING else self.MIN_RATING
+        self.level: int = 1  # initial value
+        self.max_level: int = 10 * triangular(self.rating) if self.rating < self.MAX_RATING else float('inf')
+        self.exp: mpf = mpf("0")
+        self.required_exp: mpf = mpf("1e6")
+        self.curr_hp: mpf = max_hp
+        self.max_hp: mpf = max_hp
+        self.curr_magic_points: mpf = max_magic_points
+        self.max_magic_points: mpf = max_magic_points
+        self.attack_power: mpf = attack_power
+        self.defense: mpf = defense
+        self.attack_speed: mpf = attack_speed
+        self.__skills: list = skills
+        self.awaken_bonus: AwakenBonus = awaken_bonus
+
+    def get_skills(self):
+        # type: () -> list
+        return self.__skills
+
+    # TODO: implement methods
+
+    def __str__(self):
+        # type: () -> str
+        res: str = str(type(self).__name__) + "("  # initial value
+        index: int = 0  # initial value
+        for item in vars(self).items():
+            res += str(item[0]) + "=" + str(item[1])
+
+            if index < len(vars(self).items()) - 1:
+                res += ", "
+
+            index += 1
+
+        return res + ")"
+
+    def clone(self):
+        # type: () -> LegendaryCreature
+        return copy.deepcopy(self)
+
 
 class Skill:
     """
     This class contains attributes of a skill legendary creatures have.
+    """
+
+    def __init__(self, name, description, magic_points_cost):
+        # type: (str, str, mpf) -> None
+        self.name: str = name
+        self.description: str = description
+        self.magic_points_cost: mpf = magic_points_cost
+        self.level: int = 1
+        self.is_active: bool = True
+
+    def level_up(self):
+        # type: () -> None
+        pass
+
+    def __str__(self):
+        # type: () -> str
+        res: str = str(type(self).__name__) + "("  # initial value
+        index: int = 0  # initial value
+        for item in vars(self).items():
+            res += str(item[0]) + "=" + str(item[1])
+
+            if index < len(vars(self).items()) - 1:
+                res += ", "
+
+            index += 1
+
+        return res + ")"
+
+    def clone(self):
+        # type: () -> LegendaryCreature
+        return copy.deepcopy(self)
+
+
+class BeneficialEffect:
+    """
+    This class contains attributes of a beneficial effect a legendary creature has.
+    """
+
+
+class HarmfulEffect:
+    """
+    This class contains attributes of a harmful effect a legendary creature has.
     """
 
 
@@ -458,6 +708,43 @@ class Item:
         return copy.deepcopy(self)
 
 
+class Rune(Item):
+    """
+    This class contains attributes of a rune used to strengthen legendary creatures.
+    """
+
+    MIN_SLOT_NUMBER: int = 1
+    MAX_SLOT_NUMBER: int = 6
+    MIN_RATING: int = 1
+    MAX_RATING: int = 6
+    MAX_CRIT_RATE_UP: mpf = mpf("0.85")
+
+
+class AwakenShard(Item):
+    """
+    This class contains attributes of an awaken shard to immediately awaken a legendary creature.
+    """
+
+
+class EXPShard(Item):
+    """
+    This class contains attributes of an EXP shard to increase the EXP of a legendary creature.
+    """
+
+
+class LevelUpShard(Item):
+    """
+    This class contains attributes of a shard used to immediately level up a legendary creature.
+    """
+
+
+class SkillLevelUpShard(Item):
+    """
+    This class contains attributes of a skill level up shard to immediately increase the level of a
+    skill possessed by a legendary creature.
+    """
+
+
 ###########################################
 # ITEM
 ###########################################
@@ -480,6 +767,8 @@ class Player:
         self.level: int = 1
         self.exp: mpf = mpf("0")
         self.required_exp: mpf = mpf("1e6")
+        self.gold: mpf = mpf("5e6")
+        self.gems: mpf = mpf("100")
 
     def __str__(self):
         # type: () -> str
@@ -539,11 +828,65 @@ class ItemShop:
     This class contains attributes of a shop selling items.
     """
 
+    def __init__(self, items_sold):
+        # type: (list) -> None
+        self.name: str = "ITEM SHOP"
+        self.__items_sold: list = items_sold
+
+    def get_items_sold(self):
+        # type: () -> list
+        return self.__items_sold
+
+    def __str__(self):
+        # type: () -> str
+        res: str = str(type(self).__name__) + "("  # initial value
+        index: int = 0  # initial value
+        for item in vars(self).items():
+            res += str(item[0]) + "=" + str(item[1])
+
+            if index < len(vars(self).items()) - 1:
+                res += ", "
+
+            index += 1
+
+        return res + ")"
+
+    def clone(self):
+        # type: () -> ItemShop
+        return copy.deepcopy(self)
+
 
 class BuildingShop:
     """
     This class contains attributes of a shop selling buildings.
     """
+
+    def __init__(self, buildings_sold):
+        # type: (list) -> None
+        self.name: str = "BUILDING SHOP"
+        self.__buildings_sold: list = buildings_sold
+
+    def get_buildings_sold(self):
+        # type: () -> list
+        return self.__buildings_sold
+
+    def __str__(self):
+        # type: () -> str
+        res: str = str(type(self).__name__) + "("  # initial value
+        index: int = 0  # initial value
+        for item in vars(self).items():
+            res += str(item[0]) + "=" + str(item[1])
+
+            if index < len(vars(self).items()) - 1:
+                res += ", "
+
+            index += 1
+
+        return res + ")"
+
+    def clone(self):
+        # type: () -> BuildingShop
+        return copy.deepcopy(self)
 
 
 class Reward:
@@ -551,11 +894,60 @@ class Reward:
     This class contains attributes of a reward for accomplishing something in this game.
     """
 
+    def __init__(self, player_reward_exp=mpf("0"), player_reward_gold=mpf("0"), player_reward_gems=mpf("0"),
+                 legendary_creature_reward_exp=mpf("0")):
+        # type: (mpf, mpf, mpf, mpf, mpf) -> None
+        self.player_reward_exp: mpf = player_reward_exp
+        self.player_reward_gold: mpf = player_reward_gold
+        self.player_reward_gems: mpf = player_reward_gems
+        self.legendary_creature_reward_exp: mpf = legendary_creature_reward_exp
+
+    def __str__(self):
+        # type: () -> str
+        res: str = str(type(self).__name__) + "("  # initial value
+        index: int = 0  # initial value
+        for item in vars(self).items():
+            res += str(item[0]) + "=" + str(item[1])
+
+            if index < len(vars(self).items()) - 1:
+                res += ", "
+
+            index += 1
+
+        return res + ")"
+
+    def clone(self):
+        # type: () -> Reward
+        return copy.deepcopy(self)
+
 
 class SavedGameData:
     """
     This class contains attributes of the saved game data in this game.
     """
+
+    def __init__(self, trainer_name, temperature, top_p, top_k, max_output_tokens, player_data):
+        # type: (str, float, float, float, int, Trainer) -> None
+        self.trainer_name: str = trainer_name
+        self.temperature: float = temperature
+        self.top_p: float = top_p
+        self.top_k: float = top_k
+        self.max_output_tokens: int = max_output_tokens
+        self.player_data: Player = player_data
+
+    def __str__(self):
+        # type: () -> str
+        res: str = ""  # initial value
+        res += str(self.trainer_name).upper() + "\n"
+        res += "Temperature: " + str(self.temperature) + "\n"
+        res += "Top P: " + str(self.top_p) + "\n"
+        res += "Top K: " + str(self.top_k) + "\n"
+        res += "Max output tokens: " + str(self.max_output_tokens) + "\n"
+        return res
+
+    def clone(self):
+        # type: () -> SavedGameData
+        return copy.deepcopy(self)
 
 
 ###########################################
@@ -602,11 +994,14 @@ def main() -> int:
     player_name: str = ""  # initial value
 
     # Gemini Generative Model
-    model = gemini.GenerativeModel(model_name="gemini-1.0-pro",
-                                       generation_config={"temperature": 0.9,
-                                                          "top_p": 1,
-                                                          "top_k": 1,
-                                                          "max_output_tokens": 2048,},
+    model = gemini.GenerativeModel(model_name="gemini-1.5-pro",
+                                       generation_config={
+                                           "temperature": 1,
+                                           "top_p": 0.95,
+                                           "top_k": 64,
+                                           "max_output_tokens": 8192,
+                                           "response_mime_type": "text/plain",
+                                       },
                                        safety_settings=safety_settings)  # initial value
 
     print("Enter \"NEW GAME\" to create new saved game data.")
